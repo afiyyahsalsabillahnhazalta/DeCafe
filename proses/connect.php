0960dbd3a6478136ea1d17c750f3398690a51fcb
Afiyyah Salsabillah Nhazalta <112492024+afiyyahsalsabillahnhazalta@users.noreply.github.com>
2023-10-29 13:20:28 +0700

    ubah connect.php

--- proses/connect.php
+++ proses/connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "db_decafe");
+$conn = mysqli_connect("localhost", "u837952942_yaya", "Qk?UWTX9i&20", "u837952942_yaya");
 if (!$conn){
     echo "Gagal Koneksi";
 }
